0468e7cbdf895d0953ade35e31725ab75029264c
Lodkahs <omalojiu@gmail.com>
2023-05-08 22:00:47 +0300

    Initial Commit

---
+++ VideoMedia/VideoMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoMediaApp.swift
+//  VideoMedia
+//
+//  Created by Andrew  on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
